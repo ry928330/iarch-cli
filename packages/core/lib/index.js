2f533eb9bbeb99b312b3b881ca15ff8fa68cb0e3
ranyu <ranyu@pwrd.com>
2025-09-26 14:30:54 +0800

    remove immoc-cli-ry

--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -1,10 +1,7 @@
 "use strict";
-const immocTestRy = require("immoc-test-ry");
 
 module.exports = core;
 
 function core() {
   return "Hello from core";
 }
-
-console.log(immocTestRy, core, "immocTestRy");
